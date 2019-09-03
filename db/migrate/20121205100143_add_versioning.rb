d005ac4e2cc4239a70aea3a4c06b53ad0489bb0b
FUJISE Takumi <fujise@lean-system.co.jp>
2019-09-03 17:54:30 +0900

    kb_articles テーブルに version_comments が既に存在していたので、unless column_exists? を追加

--- db/migrate/20121205100143_add_versioning.rb
+++ db/migrate/20121205100143_add_versioning.rb
@@ -1,6 +1,6 @@
 class AddVersioning < ActiveRecord::Migration[4.2]
   def self.up
-    add_column :kb_articles, :version_comments, :string, :limit => 255, :default => ""
+    add_column :kb_articles, :version_comments, :string, :limit => 255, :default => "" unless column_exists?(:kb_articles, :version_comments)
     KbArticle.create_versioned_table
     add_index KbArticle.versioned_table_name, :kb_article_id, :name => :kb_article_versions_kbaid
     add_index KbArticle.versioned_table_name, :updated_at, :name => :index_kb_article_versions_on_updated_at
